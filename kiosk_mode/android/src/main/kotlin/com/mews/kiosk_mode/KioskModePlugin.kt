5cd40de3bc476bb0234adec73f9831758d15c8f9
Rhico Abueme <63506243+rhico-real@users.noreply.github.com>
2024-08-12 10:28:31 +0800

    Update KioskModePlugin.kt

--- kiosk_mode/android/src/main/kotlin/com/mews/kiosk_mode/KioskModePlugin.kt
+++ kiosk_mode/android/src/main/kotlin/com/mews/kiosk_mode/KioskModePlugin.kt
@@ -49,19 +49,18 @@ class KioskModePlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
         activity?.let { a ->
             a.findViewById<ViewGroup>(android.R.id.content).getChildAt(0).post {
                 try {
-                    // Start Lock Task (Kiosk) Mode
-                    a.startLockTask()
-
-                    // Adjust the system UI to allow swiping from the top (status bar)
+                    // Hide the navigation and status bars
                     a.window.decorView.systemUiVisibility = (
                             View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
                             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_FULLSCREEN or
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                             )
-
-                    // Keep the screen on, and ensure the status bar is visible and swipable
+    
+                    // Keep the screen on
                     a.window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-
+    
                     result.success(true)
                     kioskModeHandler.emit()
                 } catch (e: IllegalArgumentException) {
@@ -71,13 +70,23 @@ class KioskModePlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
         } ?: result.success(false)
     }
 
-
     private fun stopKioskMode(result: MethodChannel.Result) {
-        activity?.stopLockTask()
-      
+        activity?.let { a ->
+            // Restore the system UI visibility
+            a.window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    )
+    
+            // Remove the flag to allow the screen to turn off
+            a.window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        }
+    
         result.success(null)
         kioskModeHandler.emit()
     }
+
 
     private fun isManagedKiosk(result: MethodChannel.Result) {
         val service = activity?.getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager
