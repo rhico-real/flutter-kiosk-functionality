2b08112e8c5999e12923c87de6559be17addbab3
Rhico Abueme <rhicoabuemegithub@gmail.com>
2024-08-12 15:16:44 +0800

    edit.

--- kiosk_mode/android/src/main/kotlin/com/mews/kiosk_mode/KioskModePlugin.kt
+++ kiosk_mode/android/src/main/kotlin/com/mews/kiosk_mode/KioskModePlugin.kt
@@ -1,43 +1,45 @@
 package com.mews.kiosk_mode
 
 import android.app.Activity
+import android.app.ActivityManager
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
-import android.os.Build
-import android.provider.Settings
-import android.view.Gravity
 import android.view.ViewGroup
-import android.view.WindowManager
-import android.graphics.PixelFormat
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 
-private const val REQUEST_CODE_OVERLAY_PERMISSION = 1001
+private const val methodChannelName = "com.mews.kiosk_mode/kiosk_mode"
+private const val eventChannelName = "com.mews.kiosk_mode/kiosk_mode_stream"
 
-class KioskModePlugin : FlutterPlugin, MethodChannel.MethodCallHandler, ActivityAware {
-
+class KioskModePlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     private lateinit var channel: MethodChannel
+    private lateinit var eventChannel: EventChannel
     private var activity: Activity? = null
-    private lateinit var windowManager: WindowManager
-    private var interceptView: CustomViewGroup? = null
+    private lateinit var kioskModeHandler: KioskModeStreamHandler
 
     override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "com.mews.kiosk_mode/kiosk_mode")
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, methodChannelName)
         channel.setMethodCallHandler(this)
+        kioskModeHandler = KioskModeStreamHandler(this::isInKioskMode)
+
+        eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, eventChannelName)
+        eventChannel.setStreamHandler(kioskModeHandler)
     }
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
         when (call.method) {
             "startKioskMode" -> startKioskMode(result)
             "stopKioskMode" -> stopKioskMode(result)
+            "isInKioskMode" -> isInKioskMode(result)
+            "isManagedKiosk" -> isManagedKiosk(result)
             else -> result.notImplemented()
         }
     }
-
+    
     private fun startKioskMode(result: MethodChannel.Result) {
         activity?.let { a ->
             redirectToHomeLauncherSettings(a)
@@ -56,9 +58,34 @@ class KioskModePlugin : FlutterPlugin, MethodChannel.MethodCallHandler, Activity
         } ?: result.success(false)
     }
 
-    private fun redirectToHomeLauncherSettings(activity: Activity) {
-        val intent = Intent(Settings.ACTION_HOME_SETTINGS)
-        activity.startActivity(intent)
+    private fun isManagedKiosk(result: MethodChannel.Result) {
+        val service = activity?.getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager
+        if (service == null) {
+            result.success(null)
+            return
+        }
+
+        result.success(service.lockTaskModeState == ActivityManager.LOCK_TASK_MODE_LOCKED)
+    }
+
+    private fun isInKioskMode(result: MethodChannel.Result) {
+        result.success(isInKioskMode())
+    }
+
+    private fun isInKioskMode(): Boolean? {
+        val service = activity?.getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager
+            ?: return null
+
+        return when (service.lockTaskModeState) {
+            ActivityManager.LOCK_TASK_MODE_PINNED,
+            ActivityManager.LOCK_TASK_MODE_LOCKED -> true
+            else -> false
+        }
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+        eventChannel.setStreamHandler(null)
     }
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
@@ -66,14 +93,9 @@ class KioskModePlugin : FlutterPlugin, MethodChannel.MethodCallHandler, Activity
     }
 
     override fun onDetachedFromActivityForConfigChanges() {}
-
     override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
 
     override fun onDetachedFromActivity() {
         this.activity = null
-    }
-
-    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        channel.setMethodCallHandler(null)
     }
 }
