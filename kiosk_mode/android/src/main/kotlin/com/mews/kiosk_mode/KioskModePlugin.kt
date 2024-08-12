04fc01d2e917c068109b8739a269f8d831562425
Rhico Abueme <63506243+rhico-real@users.noreply.github.com>
2024-08-12 09:13:39 +0800

    Update KioskModePlugin.kt

--- kiosk_mode/android/src/main/kotlin/com/mews/kiosk_mode/KioskModePlugin.kt
+++ kiosk_mode/android/src/main/kotlin/com/mews/kiosk_mode/KioskModePlugin.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.app.ActivityManager
 import android.content.Context
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.view.View
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -40,42 +42,23 @@ class KioskModePlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
         }
     }
 
-    // private fun startKioskMode(result: MethodChannel.Result) {
-    //     activity?.let { a ->
-    //         // ensures that startLockTask() will not throw
-    //         // see https://stackoverflow.com/questions/27826431/activity-startlocktask-occasionally-throws-illegalargumentexception
-    //         a.findViewById<ViewGroup>(android.R.id.content).getChildAt(0).post {
-    //             try {
-    //                 a.startLockTask()
-    //                 result.success(true)
-    //                 kioskModeHandler.emit()
-    //             } catch (e: IllegalArgumentException) {
-    //                 result.success(false)
-    //             }
-    //         }
-    //     } ?: result.success(false)
-    // }
-
     private fun startKioskMode(result: MethodChannel.Result) {
         activity?.let { a ->
-            // Ensures that startLockTask() will not throw
-            // See https://stackoverflow.com/questions/27826431/activity-startlocktask-occasionally-throws-illegalargumentexception
             a.findViewById<ViewGroup>(android.R.id.content).getChildAt(0).post {
                 try {
                     // Start Lock Task (Kiosk) Mode
                     a.startLockTask()
-    
-                    // Adjust the system UI to allow swiping from the top
+
+                    // Adjust the system UI to allow swiping from the top (status bar)
                     a.window.decorView.systemUiVisibility = (
                             View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
                             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
-                            View.SYSTEM_UI_FLAG_FULLSCREEN or
                             View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             )
-    
-                    // Ensure the status bar is visible and swipable
+
+                    // Keep the screen on, and ensure the status bar is visible and swipable
                     a.window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-    
+
                     result.success(true)
                     kioskModeHandler.emit()
                 } catch (e: IllegalArgumentException) {
@@ -85,13 +68,11 @@ class KioskModePlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
         } ?: result.success(false)
     }
 
-
     private fun stopKioskMode(result: MethodChannel.Result) {
         activity?.stopLockTask()
         result.success(null)
         kioskModeHandler.emit()
     }
-
 
     private fun isManagedKiosk(result: MethodChannel.Result) {
         val service = activity?.getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager
